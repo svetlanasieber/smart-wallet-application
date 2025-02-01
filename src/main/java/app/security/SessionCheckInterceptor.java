62a92c141e90d09329d3350f621dae7e35e3181b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-02 00:47:44 +0100

    Update SessionCheckInterceptor.java

--- src/main/java/app/security/SessionCheckInterceptor.java
+++ src/main/java/app/security/SessionCheckInterceptor.java
@@ -1,1 +1,35 @@
+package app.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Set;
+
+@Component
+public class SessionCheckInterceptor implements HandlerInterceptor {
+
+    private final Set<String> UNAUTHENTICATED_ENDPOINTS = Set.of("/", "/login", "/register");
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+ 
+        String endpoint = request.getServletPath();
+        if (UNAUTHENTICATED_ENDPOINTS.contains(endpoint)) {
+    
+            return true;
+        }
+
+        HttpSession currentUserSession = request.getSession(false);
+        if (currentUserSession == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+
+        return true;
+    }
+}
 
